09d1ad6d12fea5c13ed1a832143911f454e1a851
Petr Krysl <pkrysl@ucsd.edu>
2019-07-29 21:08:38 +0200

    Additional tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,4 +6,5 @@ using Test
 @time @testset "Linear deformation 5" begin include("test_linear_defor_5.jl") end
 @time @testset "Linear deformation 6" begin include("test_linear_defor_6.jl") end
 @time @testset "Linear deformation 7" begin include("test_linear_defor_7.jl") end
+@time @testset "Linear deformation 8" begin include("test_linear_defor_8.jl") end
 true
