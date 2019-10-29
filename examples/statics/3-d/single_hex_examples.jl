099ba1483cb5f8507461989bd32e78ac4eb3c3a9
Petr Krysl <pkrysl@ucsd.edu>
2019-10-29 13:40:01 +0100

    Additional element type

--- examples/statics/3-d/single_hex_examples.jl
+++ examples/statics/3-d/single_hex_examples.jl
@@ -10,20 +10,47 @@ using LinearAlgebra: Symmetric, cholesky, eigen
 # Isotropic material
 E = 1.0;
 nu = 0.3
-a, b, c = 1.0, 1.0, 1.0
 # julia> rand(8,3) .* 2 .- 1.0           
 perturbation = [         
-  0.0767656  -0.983206    -0.14343     
-  0.45767     0.981479     0.450997    
- -0.295854    0.542922     0.321333    
- -0.85204    -0.97824     -0.772874    
- -0.0539756   0.994907     0.822798    
-  0.447173    0.528742     0.0445352   
- -0.468417    0.00673427   0.953151    
- -0.898513   -0.915871     0.933237  ] ./ 10.0
+0.0767656  -0.983206    -0.14343     
+0.45767     0.981479     0.450997    
+-0.295854    0.542922     0.321333    
+-0.85204    -0.97824     -0.772874    
+-0.0539756   0.994907     0.822798    
+0.447173    0.528742     0.0445352   
+-0.468417    0.00673427   0.953151    
+-0.898513   -0.915871     0.933237  ] ./ 5.0
+# Lambda1 = vec([-1.0 1.0 1.0 -1.0 -1.0 1.0 1.0 -1.0])
+# Lambda2 = vec([-1.0 -1.0 1.0 1.0 -1.0 -1.0 1.0 1.0])
+# Gamma1 = vec([1.0 1.0 -1.0 -1.0 -1.0 -1.0 1.0 1.0])
+# perturbation = zeros((8,3))  
+# perturbation[:, 1] .= Lambda2 ./ 10.0
+# perturbation[:, 2] .= Lambda1 ./ 10.0
+# perturbation[:, 3] .= Gamma1 ./ 20.0
+
+mesh() = (FinEtools.FENodeSetModule.FENodeSet([
+	0.0 0.0 0.0; 
+	1.0 0.0 0.0; 
+	1.0 1.0 0.0; 	
+	0.0 1.0 0.0; 
+	0.0 0.0 1.0;
+	1.0 0.0 1.0; 
+	1.0 1.0 1.0
+	0.0 1.0 1.0; 
+	]), FinEtools.FESetModule.FESetH8(reshape([1, 2, 3, 4, 5, 6, 7, 8], 1, 8)))
+
+function single_hex_perfect_cube()
+	fens,fes = mesh()
+
+	File =  "single_hex_perfect_cube.vtk"
+	vtkexportmesh(File, fens, fes)
+	@async run(`"paraview.exe" $File`)
+
+	true
+end # single_hex_perfect_cube
 
 function single_hex_full()
-   	fens,fes = H8block(a, b, c, 1, 1, 1)
+   	fens,fes = mesh()
     fens.xyz +=  perturbation
     
     MR = DeforModelRed3D
@@ -53,7 +80,7 @@ function single_hex_full()
 end # single_hex_full
 
 function single_hex_underintegrated()
-   	fens,fes = H8block(a, b, c, 1, 1, 1)
+   	fens,fes = mesh()
     fens.xyz +=  perturbation
     
     MR = DeforModelRed3D
@@ -82,13 +109,50 @@ function single_hex_underintegrated()
 
 end # single_hex_underintegrated
 
+function single_hex_ms()
+   	fens,fes = mesh()
+    fens.xyz +=  perturbation
+    
+    MR = DeforModelRed3D
+    material = MatDeforElastIso(MR, E, nu)
+
+    geom = NodalField(fens.xyz)
+    u = NodalField(zeros(size(fens.xyz,1), 3)) # displacement field
+	applyebc!(u)
+    numberdofs!(u)
+
+    femm = FEMMDeforLinearMSH8(MR, IntegDomain(fes, GaussRule(3, 2)), material)
+
+    @show vol = integratefunction(femm, geom, x -> 1.0, 3)
+
+    associategeometry!(femm, geom)
+    fill!(femm.phis, 0.0)
+    K = stiffness(femm, geom, u)
+
+    D = eigen(Matrix(K))
+    
+    File =  "single_hex_ms.vtk"
+    vectors = [("ev_$(idx)_$(round(D.values[idx] * 10000) / 10000)", deepcopy(scattersysvec!(u, D.vectors[:,idx]).values)) for idx in 1:length(D.values)] 
+    vtkexportmesh(File, fens, fes;  vectors=vectors)
+    @async run(`"paraview.exe" $File`)
+
+    true
+
+end # single_hex_ms
+
 function allrun()
+	println("#####################################################")
+	println("# single_hex_perfect_cube ")
+	single_hex_perfect_cube()
     println("#####################################################")
     println("# single_hex_full ")
     single_hex_full()
     println("#####################################################")
     println("# single_hex_underintegrated ")
     single_hex_underintegrated()
+    println("#####################################################")
+    println("# single_hex_ms ")
+    single_hex_ms()
     return true
 end # function allrun
 
