722dc65007e036f86ea2c8c6f7bf78c3100645c2
Petr Krysl <pkrysl@ucsd.edu>
2019-11-11 19:58:12 +0100

    fix test

--- test/test_linear_defor_9.jl
+++ test/test_linear_defor_9.jl
@@ -1,6 +1,6 @@
 module m4thsymmcheck1
 using FinEtools
-using FinEtoolsDeforLinear: tens4checksymmetry, tens4symm6x6tot!, tens4symmtto6x6t!
+using FinEtoolsDeforLinear: tens4checksymmetry, tens4symmt6x6tot!, tens4symmtto6x6t!
 using LinearAlgebra
 using Test
 function test()
@@ -8,7 +8,7 @@ function test()
 	C = C + C'
 	Co = deepcopy(C)
 	t = fill(0.0, 3, 3, 3, 3)
-	tens4symm6x6tot!(t, C)
+	tens4symmt6x6tot!(t, C)
 	@test tens4checksymmetry(t)
 	tens4symmtto6x6t!(C, t)
 	@assert norm(C - C') <= eps(1.0)
