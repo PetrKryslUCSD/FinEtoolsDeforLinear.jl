2ec4be4d5285bacb5e8576d7f6bde5c4c0746c6b
Petr Krysl <pkrysl@ucsd.edu>
2019-11-19 18:50:50 +0100

    shortcut

--- go.jl
+++ go.jl
@@ -4,3 +4,6 @@ Pkg.instantiate()
 using FinEtoolsDeforLinear
 
 # Pkg.test()
+include("examples/statics/3-d/bend_hex_spectrum_examples.jl"); bend_hex_spectrum_examples.allrun()
+include("examples/statics/3-d/comp_hex_spectrum_examples.jl"); comp_hex_spectrum_examples.allrun()
+
