e11b67bf65015601e794d8967315888df5fdce02
Petr Krysl <pkrysl@ucsd.edu>
2019-11-20 18:34:00 +0100

    with perturbation

--- examples/singleel/bend_hex_spectrum_examples.jl
+++ examples/singleel/bend_hex_spectrum_examples.jl
@@ -14,15 +14,15 @@ E = 1.0;
 nu = 0.3
 aspects = [1.0 10.0 100.0 1000.0]
 # julia> rand(8,3) .* 2 .- 1.0           
-# xyzperturbation = [         
-# 0.0767656  -0.983206    -0.14343     
-# 0.45767     0.981479     0.450997    
-# -0.295854    0.542922     0.321333    
-# -0.85204    -0.97824     -0.772874    
-# -0.0539756   0.994907     0.822798    
-# 0.447173    0.528742     0.0445352   
-# -0.468417    0.00673427   0.953151    
-# -0.898513   -0.915871     0.933237  ] ./ 15.0
+xyzperturbation = [         
+0.0767656  -0.983206    -0.14343     
+0.45767     0.981479     0.450997    
+-0.295854    0.542922     0.321333    
+-0.85204    -0.97824     -0.772874    
+-0.0539756   0.994907     0.822798    
+0.447173    0.528742     0.0445352   
+-0.468417    0.00673427   0.953151    
+-0.898513   -0.915871     0.933237  ] ./ 15.0
 # Lambda1 = vec([-1.0 1.0 1.0 -1.0 -1.0 1.0 1.0 -1.0])
 # Lambda2 = vec([-1.0 -1.0 1.0 1.0 -1.0 -1.0 1.0 1.0])
 # Gamma1 = vec([1.0 1.0 -1.0 -1.0 -1.0 -1.0 1.0 1.0])
@@ -45,6 +45,7 @@ mesh() = (FinEtools.FENodeSetModule.FENodeSet([
 function bend_hex_spectrum_full()
 	function sim(aspect)
 		fens,fes = mesh()
+		fens.xyz +=  xyzperturbation
 		fens.xyz[:, 1] .*= aspect
 
 		MR = DeforModelRed3D
@@ -64,10 +65,10 @@ function bend_hex_spectrum_full()
 
 		D = eigen(Matrix(K))
 
-		# File =  "bend_hex_spectrum_full.vtk"
-		# vectors = [("ev_$(idx)_$(round(D.values[idx] * 10000) / 10000)", deepcopy(scattersysvec!(u, D.vectors[:,idx]).values)) for idx in 1:length(D.values)] 
-		# vtkexportmesh(File, fens, fes;  vectors=vectors)
-		# @async run(`"paraview.exe" $File`)
+		File =  "bend_hex_spectrum_full.vtk"
+		vectors = [("ev_$(idx)_$(round(D.values[idx] * 10000) / 10000)", deepcopy(scattersysvec!(u, D.vectors[:,idx]).values)) for idx in 1:length(D.values)] 
+		vtkexportmesh(File, fens, fes;  vectors=vectors)
+		@async run(`"paraview.exe" $File`)
 
 		savecsv("bend_hex_spectrum_full-aspect=$(aspect).csv", eigenvalues = vec(D.values))
 		# @pgf _a = SemiLogYAxis({
@@ -163,6 +164,7 @@ end # bend_hex_spectrum_ms
 function bend_hex_spectrum_im()
 	function sim(aspect)
 		fens,fes = mesh()
+		fens.xyz +=  xyzperturbation
 		fens.xyz[:, 1] .*= aspect
 
 		MR = DeforModelRed3D
@@ -173,7 +175,7 @@ function bend_hex_spectrum_im()
 		applyebc!(u)
 		numberdofs!(u)
 
-		femm = FEMMDeforLinearIMH8(MR, IntegDomain(fes, GaussRule(3, 2)), material, 12)
+		femm = FEMMDeforLinearIMH8(MR, IntegDomain(fes, GaussRule(3, 2)), material, 9)
 
 		vol = integratefunction(femm, geom, x -> 1.0, 3)
 
