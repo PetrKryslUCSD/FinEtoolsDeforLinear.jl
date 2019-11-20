0c07f2c3266dd345d972584ea93ce52eaebb9786
Petr Krysl <pkrysl@ucsd.edu>
2019-11-20 18:34:23 +0100

    nomenclature

--- examples/singleel/hex_ip_constr_examples.jl
+++ examples/singleel/hex_ip_constr_examples.jl
@@ -20,66 +20,75 @@ mesh() = (FinEtools.FENodeSetModule.FENodeSet([
 	0.0 1.0 1.0; 
 	]), FinEtools.FESetModule.FESetH8(reshape([1, 2, 3, 4, 5, 6, 7, 8], 1, 8)))
 
-function hex_ip_constr_full()
+function hex_ip_constr_par()
 	fens,fes = mesh()
 
-	# MR = DeforModelRed3D
-	# material = MatDeforElastIso(MR, E, nu)
-
-	# geom = NodalField(fens.xyz)
-	# u = NodalField(zeros(size(fens.xyz,1), 3)) # displacement field
-	# applyebc!(u)
-	# numberdofs!(u)
-
-	integration_rule = TrapezoidalRule(3)
+	# integration_rule = TrapezoidalRule(3)
+	integration_rule = GaussRule(3, 1)
 	pc = integration_rule.param_coords;
-	w  =  integration_rule.weights ;
+	w  =  integration_rule.weights
 	npts = integration_rule.npts;
 
-	C = fill(0.0, npts, 24)
+	Q = fill(0.0, npts, 24)
 	for j in 1:npts
-		gradN = bfundpar(fes, vec(pc[j,:]))
+		gradNpar = bfundpar(fes, vec(pc[j,:]))
 		k = 1
-		for i in 1:size(gradN, 1)
-			C[j, k:(k+2)] .= gradN[i, :]
+		for i in 1:size(gradNpar, 1)
+			Q[j, k:(k+2)] .= gradNpar[i, :]
 			k = k + 3
 		end
 	end
 
-	decomp = svd(C)
+	decomp = svd(Q)
 	@show decomp.S
-	# femm = FEMMDeforLinear(MR, IntegDomain(fes, ), material)
-
-	# vol = integratefunction(femm, geom, x -> 1.0, 3)
-
-	# associategeometry!(femm, geom)
-	# K = stiffness(femm, geom, u)
-
-	# D = eigen(Matrix(K))
-
-		# File =  "comp_hex_spectrum_full.vtk"
-		# vectors = [("ev_$(idx)_$(round(D.values[idx] * 10000) / 10000)", deepcopy(scattersysvec!(u, D.vectors[:,idx]).values)) for idx in 1:length(D.values)] 
-		# vtkexportmesh(File, fens, fes;  vectors=vectors)
-		# @async run(`"paraview.exe" $File`)
-
-		# savecsv("comp_hex_spectrum_full-nu=$(nu).csv", eigenvalues = vec(D.values))
-		# @pgf _a = SemiLogYAxis({
-		#     xlabel = "Mode [ND]",
-		#     ylabel = "Generalized stiffness [N/m]",
-		#     grid="major",
-		#     legend_pos  = "south east",
-		#     title = "Hexahedron spectrum, \\nu=$(nu)"
-		# },
-		# Plot({"red", mark="triangle"}, Table([:x => vec(7:size(K, 1)), :y => vec(D.values[7:end])])), LegendEntry("FEA"))
-		# display(_a)
-
+	
 		true
-end # hex_ip_constr_full
+end # hex_ip_constr_par
+
+function hex_ip_constr_xyz()
+	xyzperturbation = [         
+	0.0767656  -0.983206    -0.14343     
+	0.45767     0.981479     0.450997    
+	-0.295854    0.542922     0.321333    
+	-0.85204    -0.97824     -0.772874    
+	-0.0539756   0.994907     0.822798    
+	0.447173    0.528742     0.0445352   
+	-0.468417    0.00673427   0.953151    
+	-0.898513   -0.915871     0.933237  ] ./ 5.0
+	fens,fes = mesh()
+	fens.xyz .+= xyzperturbation
+
+	# integration_rule = TrapezoidalRule(3)
+	integration_rule = GaussRule(3, 2)
+	pc = integration_rule.param_coords;
+	w  =  integration_rule.weights
+	npts = integration_rule.npts;
+
+	Q = fill(0.0, npts, 24)
+	for j in 1:npts
+		gradNpar = bfundpar(fes, vec(pc[j,:]))
+		J = (transpose(fens.xyz) * gradNpar)
+		@show gradN = gradNpar / J
+		k = 1
+		for i in 1:size(gradN, 1)
+			Q[j, k:(k+2)] .= gradN[i, :]
+			k = k + 3
+		end
+	end
+
+	decomp = svd(Q)
+	@show decomp.S
+	
+	true
+end # hex_ip_constr_xyz
 
 function allrun()
 	println("#####################################################")
-	println("# hex_ip_constr_full ")
-	hex_ip_constr_full()
+	println("# hex_ip_constr_par ")
+	hex_ip_constr_par()
+	println("#####################################################")
+	println("# hex_ip_constr_xyz ")
+	hex_ip_constr_xyz()
 	return true
 end # function allrun
 
