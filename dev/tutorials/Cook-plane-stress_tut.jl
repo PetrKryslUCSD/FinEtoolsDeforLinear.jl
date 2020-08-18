db7a8a21a31d488f3d383c325efa4ef7236fe9a2
Documenter.jl <documenter@juliadocs.github.io>
2020-08-18 22:45:22 +0000

    build based on a7aaf4f

--- dev/tutorials/Cook-plane-stress_tut.jl
+++ dev/tutorials/Cook-plane-stress_tut.jl
@@ -1,15 +1,32 @@
 # # Cook panel under plane stress
 
-# In this example we investigate the well-known benchmark of a tapered panel under plane stress conditions known under the name of Cook.  The problem has been solved many times with a variety of finite element models  and hence the solution is well-known.
+# ## Description
+
+# In this example we investigate the well-known benchmark of a tapered panel
+# under plane stress conditions known under the name of Cook.  The problem has
+# been solved many times with a variety of finite element models  and hence the
+# solution is well-known.
+
+# ## Goals
+
+# - Show how to generate the mesh.
+# - Execute the simulation with a static-equilibrium algorithm (solver).
+
+##
+# ## Definitions
 
 # The problem is solved in a script.  We begin  by `using` the top-level module `FinEtools`. Further, we use the linear-deformation application package.
 using FinEtools
 using FinEtoolsDeforLinear
 
-# With the algorithm modules, the problem can be set up (the materials, boundary conditions, and mesh are defined) and handed off to an algorithm (in this case linear static solution).  Then for postprocessing another set of algorithms can be invoked.
+# With the algorithm modules, the problem can be set up (the materials, boundary
+# conditions, and mesh are defined) and handed off to an algorithm (in this
+# case linear static solution).  Then for postprocessing another set of
+# algorithms can be invoked.
 using FinEtoolsDeforLinear.AlgoDeforLinearModule
 
-# A few  input parameters are defined: the material parameters. Note: the units are consistent, but unnamed.
+# A few  input parameters are defined: the material parameters. Note: the units
+# are consistent, but unnamed.
 E = 1.0;
 nu = 1.0/3;
 
@@ -19,27 +36,35 @@ free_height  = 16.0;
 # Location of tracked  deflection is the midpoint of the loaded edge.
 Mid_edge  = [48.0, 52.0];
 
-# The tapered panel is loaded along the free edge with a unit force, which is here converted to loading per unit area.
+# The tapered panel is loaded along the free edge with a unit force, which is
+# here converted to loading per unit area.
 magn = 1.0/free_height/thickness;# Magnitude of applied load
 
-# For the above input parameters the converged displacement of the tip  of the tapered panel in the direction of the applied shear load is
+# For the above input parameters the converged displacement of the tip  of the
+# tapered panel in the direction of the applied shear load is
 convutip = 23.97;
 
-# The mesh is generated as a rectangular block to begin with, and then the coordinates of the nodes are tweaked into the tapered panel shape. In this case we are using quadratic triangles (T6).
+# The mesh is generated as a rectangular block to begin with, and then the
+# coordinates of the nodes are tweaked into the tapered panel shape. In this
+# case we are using quadratic triangles (T6).
 n = 10; # number of elements per side
-fens,fes = T6block(width, height, n, n)
+fens, fes = T6block(width, height, n, n)
 
 # Reshape the rectangle into a trapezoidal panel:
-for i = 1:count(fens)
-    fens.xyz[i,2] = fens.xyz[i,2]+(fens.xyz[i,1]/width)*(height -fens.xyz[i,2]/height*(height-free_height));
+for i in 1:count(fens)
+    fens.xyz[i,2] += (fens.xyz[i,1]/width)*(height -fens.xyz[i,2]/height*(height-free_height));
 end
 
-# The  boundary conditions  are applied to selected finite element nodes.   The selection is based on the inclusion in a selection "box".
+# The  boundary conditions  are applied to selected finite element nodes.   The
+# selection is based on the inclusion in a selection "box".
 tolerance = minimum([width, height])/n/1000.;#Geometrical tolerance
 # Clamped edge of the membrane
 l1 = selectnode(fens; box=[0.,0.,-Inf, Inf], inflate = tolerance);
 
-# The list of the selected nodes is then used twice,  to fix the degree of freedom  in the direction 1 and  in the direction 2. The essential-boundary condition data is stored in  dictionaries: `ess1` and `ess2 `.  These dictionaries  are used below to compose the computational model.
+# The list of the selected nodes is then used twice,  to fix the degree of
+# freedom  in the direction 1 and  in the direction 2. The essential-boundary
+# condition data is stored in  dictionaries: `ess1` and `ess2 `.  These
+# dictionaries  are used below to compose the computational model.
 ess1 = FDataDict("displacement"=>  0.0, "component"=> 1, "node_list"=>l1);
 ess2 = FDataDict("displacement"=>  0.0, "component"=> 2, "node_list"=>l1);
 
@@ -47,10 +72,17 @@ ess2 = FDataDict("displacement"=>  0.0, "component"=> 2, "node_list"=>l1);
 
 boundaryfes =  meshboundary(fes);
 
-# Then from these finite elements we choose the ones that are inside the box that captures the edge of the geometry to which the traction should be applied.
+# Then from these finite elements we choose the ones that are inside the box
+# that captures the edge of the geometry to which the traction should be
+# applied.
 Toplist  = selectelem(fens, boundaryfes, box= [width, width, -Inf, Inf ], inflate=  tolerance);
 
-# To apply the traction we create a finite element model machine (FEMM). For the evaluation of the traction it is sufficient to create a  "base" FEMM.  It consists of the geometry data `IntegDomain` (connectivity,  integration rule, evaluation  of the basis functions  and basis function gradients with respect to the parametric coordinates). This object is composed of the list of the finite elements and  an appropriate quadrature rule (Gauss rule here).
+# To apply the traction we create a finite element model machine (FEMM). For the
+# evaluation of the traction it is sufficient to create a  "base" FEMM.  It
+# consists of the geometry data `IntegDomain` (connectivity,  integration rule,
+# evaluation  of the basis functions  and basis function gradients with respect
+# to the parametric coordinates). This object is composed of the list of the
+# finite elements and  an appropriate quadrature rule (Gauss rule here).
 el1femm = FEMMBase(IntegDomain(subset(boundaryfes, Toplist), GaussRule(1, 3), thickness));
 
 # The traction boundary condition is specified with a constant traction vector and the FEMM that will be used to evaluate  the load vector.
@@ -58,12 +90,20 @@ flux1 = FDataDict("traction_vector"=>[0.0,+magn],
     "femm"=>el1femm
     );
 
-# We make the dictionary for the region (the interior of the domain).  The FEMM for the evaluation of the integrals over the interior of the domain (that is the stiffness matrix) and the material are needed. The geometry data  now is equipped with the  triangular  three-point rule. Note the model-reduction type which is used to dispatch to appropriate specializations of the material routines and the FEMM which needs to execute different code for different reduced-dimension models. Here the model reduction is "plane stress".
+# We make the dictionary for the region (the interior of the domain).  The FEMM
+# for the evaluation of the integrals over the interior of the domain (that is
+# the stiffness matrix) and the material are needed. The geometry data  now is
+# equipped with the  triangular  three-point rule. Note the model-reduction
+# type which is used to dispatch to appropriate specializations of the material
+# routines and the FEMM which needs to execute different code for different
+# reduced-dimension models. Here the model reduction is "plane stress".
 MR = DeforModelRed2DStress
 material = MatDeforElastIso(MR,  0.0, E, nu, 0.0)
 region1 = FDataDict("femm"=>FEMMDeforLinear(MR, IntegDomain(fes, TriRule(3), thickness), material));
 
-# The model data is a dictionary.   In the present example it consists of the node set, the array of dictionaries for the regions, and arrays of dictionaries for each essential and natural boundary condition.
+# The model data is a dictionary.   In the present example it consists of the
+# node set, the array of dictionaries for the regions, and arrays of
+# dictionaries for each essential and natural boundary condition.
 modeldata = FDataDict("fens"=>fens,
  "regions"=>[region1],
  "essential_bcs"=>[ess1, ess2],
@@ -73,39 +113,51 @@ modeldata = FDataDict("fens"=>fens,
 # When the model data is defined, we simply pass it to the algorithm.
 modeldata = AlgoDeforLinearModule.linearstatics(modeldata);
 
-# The model data is augmented in the algorithm by the nodal field representing the geometry and the displacement field  computed by solving the system of linear algebraic equations of equilibrium.
+# The model data is augmented in the algorithm by the nodal field representing
+# the geometry and the displacement field  computed by solving the system of
+# linear algebraic equations of equilibrium.
 u = modeldata["u"];
 geom = modeldata["geom"];
 
 # The complete information returned from the algorithm  is
 @show keys(modeldata)
 
-# Now we can extract the displacement at the mid-edge node and compare to the converged (reference) value. The code below selects the node inside a very small box of the size `tolerance` which presumably contains only a single node, the one at the midpoint of the edge.
+# Now we can extract the displacement at the mid-edge node and compare to the
+# converged (reference) value. The code below selects the node inside a very
+# small box of the size `tolerance` which presumably contains only a single
+# node, the one at the midpoint of the edge.
 nl = selectnode(fens, box=[Mid_edge[1],Mid_edge[1],Mid_edge[2],Mid_edge[2]],
           inflate=tolerance);
 theutip = u.values[nl,:]
 println("displacement =$(theutip[2]) as compared to converged $convutip")
 
-# For postprocessing  we will export a VTK file  with the displacement field (vectors)  and  one scalar field ($\sigma_{xy}$).
+# For postprocessing  we will export a VTK file  with the displacement field
+# (vectors)  and  one scalar field ($\sigma_{xy}$).
 modeldata["postprocessing"] = FDataDict("file"=>"cookstress",
    "quantity"=>:Cauchy, "component"=>:xy);
 modeldata = AlgoDeforLinearModule.exportstress(modeldata);
 
-# The  attribute `"postprocessing"` holds additional data computed and returned by the algorithm:
+# The  attribute `"postprocessing"` holds additional data computed and returned
+# by the algorithm:
 @show keys(modeldata["postprocessing"])
 
-# The exported data can be digested as follows: `modeldata["postprocessing"]["exported"]` is an array of exported items.
+# The exported data can be digested as follows: `modeldata["postprocessing"]
+# ["exported"]` is an array of exported items.
 display(keys(modeldata["postprocessing"]["exported"]))
 
 # Each entry of the array is a dictionary:
 display(keys(modeldata["postprocessing"]["exported"][1]))
 
-# Provided we have  `paraview` in the PATH, we can bring it up  to display the exported data.
+# Provided we have  `paraview` in the PATH, we can bring it up  to display the
+# exported data.
 File = modeldata["postprocessing"]["exported"][1]["file"]
 @async run(`"paraview.exe" $File`);
 
-# We can also extract the minimum and maximum value of the shear stress (-0.06, and 0.12).
+# We can also extract the minimum and maximum value of the shear stress
+# (-0.06, and 0.12).
 display(modeldata["postprocessing"]["exported"][1]["quantity"])
 display(modeldata["postprocessing"]["exported"][1]["component"])
 fld = modeldata["postprocessing"]["exported"][1]["field"]
 println("$(minimum(fld.values)) $(maximum(fld.values))")
+
+true
--- dev/tutorials/TEST13H_tut.jl
+++ dev/tutorials/TEST13H_tut.jl
@@ -18,7 +18,7 @@
 # The nonzero benchmark frequencies are (in hertz): 2.377, 5.961, 5.961, 9.483,
 # 12.133, 12.133, 15.468, 15.468 [Hz].
 
-![](test13h_real_imag.png)
+# ![](test13h_real_imag.png)
 
 # ## References
 
@@ -57,17 +57,22 @@ frequencies = vcat(linearspace(0.0,2.377,20), linearspace(2.377,15.0,70))
 # where $m=1,2$.  Solving for the Rayleigh parameters $a_0,a_1$ yields:
 zeta1 = 0.02; zeta2 = 0.02;
 o1 = 2*pi*2.377;  o2 = 2*pi*15.468;
-Rayleigh_mass = 2*(o1*o2)/(o2^2-o1^2)*(o2*zeta1-o1*zeta2);# a0
-Rayleigh_stiffness = 2*(o1*o2)/(o2^2-o1^2)*(-1/o2*zeta1+1/o1*zeta2);# a1
+a0 = 2*(o1*o2)/(o2^2-o1^2)*(o2*zeta1-o1*zeta2);# a0
+a1 = 2*(o1*o2)/(o2^2-o1^2)*(-1/o2*zeta1+1/o1*zeta2);# a1
 
-Rayleigh_mass = Rayleigh_mass;
-Rayleigh_stiffness = Rayleigh_stiffness;
+##
+# ## Discrete model
 
-MR = DeforModelRed3D
+# Generate the finite element domain as a block.
 fens,fes  = H8block(L, L, t, nL, nL, nt)
 
+# Create the geometry field.
 geom = NodalField(fens.xyz)
+# Create the displacement field. Note that it holds complex numbers.
 u = NodalField(zeros(FCplxFlt, size(fens.xyz,1), 3)) # displacement field
+
+# In order to apply the essential boundary conditions we need to select the
+# nodes along the side faces of the plate and support them in the Z direction.
 nl = selectnode(fens, box=[0.0 0.0 -Inf Inf -Inf Inf], inflate=tolerance)
 setebc!(u, nl, true, 3)
 nl = selectnode(fens, box=[L L -Inf Inf -Inf Inf], inflate=tolerance)
@@ -76,31 +81,65 @@ nl = selectnode(fens, box=[-Inf Inf 0.0 0.0 -Inf Inf], inflate=tolerance)
 setebc!(u, nl, true, 3)
 nl = selectnode(fens, box=[-Inf Inf L L -Inf Inf], inflate=tolerance)
 setebc!(u, nl, true, 3)
+# Those boundary conditions can now be applied to the displacement field,...
 applyebc!(u)
+# ... and the degrees of freedom can be numbered.
 numberdofs!(u)
 println("nfreedofs = $(u.nfreedofs)")
     
+# The model is three-dimensional.
+MR = DeforModelRed3D
 material = MatDeforElastIso(MR, rho, E, nu, 0.0)
     
+# Given how relatively thin the plate is we choose an effective element: the
+# mean-strain hexahedral element which is quite tolerant of the high aspect
+# ratio.
 femm = FEMMDeforLinearMSH8(MR, IntegDomain(fes, GaussRule(3,2)), material)
+# These elements require  to know the geometry before anything else can be
+# computed using them in a finite element machine. Hence we first need to
+# associate the geometry with the FEMM.
 femm = associategeometry!(femm, geom)
+# Now we can calculate the stiffness matrix and the mass matrix: both evaluated
+# with the high-order Gauss rule.
 K = stiffness(femm, geom, u)
 femm = FEMMDeforLinear(MR, IntegDomain(fes, GaussRule(3,3)), material)
 M = mass(femm, geom, u)
-C = Rayleigh_mass*M + Rayleigh_stiffness*K
+# The damping matrix is a linear combination of the mass matrix and the
+# stiffness matrix (Rayleigh model).
+C = a0*M + a1*K
     
+# Find the boundary finite elements on top of the plate. The uniform distributed
+# loading will be applied to these elements.
 bdryfes = meshboundary(fes)
+# Those facing up (in the positive Z direction) will be chosen:
 topbfl = selectelem(fens, bdryfes, facing=true, direction=[0.0 0.0 1.0])
-el1femm =  FEMMBase(IntegDomain(subset(bdryfes,topbfl), GaussRule(2,2)))
+# A base finite element model machine will be created to evaluate the loading.
+# The force intensity is created as driven by a function, but the function
+# really only just fills the buffer with the constant loading vector.
 function pfun(forceout::FVec{T}, XYZ::FFltMat, tangents::FFltMat, fe_label::FInt) where {T}
     forceout .=  [0.0, 0.0, -qmagn]
     return forceout
 end
 fi = ForceIntensity(FFlt, 3, pfun);
+
+# The loading vector is lumped from the distributed uniform loading by
+# integrating on the boundary. Hence, the dimension of the integration domain
+# is 2.
+el1femm =  FEMMBase(IntegDomain(subset(bdryfes,topbfl), GaussRule(2,2)))
 F = distribloads(el1femm, geom, u, fi, 2);
 
-print("Sweeping through $(length(frequencies))\n")
+##
+# ## Sweep through the frequencies
+
+
+# Sweep through the frequencies and calculate the complex displacement vector
+# for each  of the frequencies from the complex balance equations of the
+# structure.
+
+# The entire solution will be stored  in this array:
 U1 = zeros(FCplxFlt, u.nfreedofs, length(frequencies))
+
+print("Sweeping through $(length(frequencies)) frequencies\n")
 for k in 1:length(frequencies)
     frequency = frequencies[k];
     omega = 2*pi*frequency;
@@ -109,72 +148,55 @@ for k in 1:length(frequencies)
 end
 print("\n")
 
+# Find the midpoint of the plate bottom surface.  For this purpose the number of
+# elements along the edge of the plate needs to be divisible by two.
 midpoint = selectnode(fens, box=[L/2 L/2 L/2 L/2 0 0], inflate=tolerance);
+# Check that we found that node.
+@assert midpoint != []
+# Extract the displacement component in the vertical direction (Z).
 midpointdof = u.dofnums[midpoint, 3]
 
-using PlotlyJS
-umidAmpl = abs.(U1[midpointdof, :])/phun("MM")
-# @pgf _a = SemiLogXAxis({
-#     xlabel = "Frequency [Hz]",
-#     ylabel = "Midpoint  displacement amplitude [mm]",
-#     grid="major",
-#     legend_pos  = "south east",
-#     title = "Thin plate midpoint Amplitude FRF"
-# },
-# Plot({"red", mark="triangle"}, Table([:x => vec(frequencies), :y => vec(umidAmpl)])), LegendEntry("FEA"))
-# display(_a)
+##
+# ## Plot the results
 
-# Set up the layout:
-layout = Layout(;width=650, height=600, xaxis=attr(title="Frequency [Hz]", type = "log"), yaxis=attr(title="Midpoint  displacement amplitude [mm]", type = "linear"), title = "Thin plate midpoint Amplitude FRF")
-# Plot the graphs:
-plots = cat(scatter(;x=vec(frequencies), y=vec(umidAmpl), mode="lines", name = "FEA", line_color = "rgb(215, 15, 15)"); dims = 1)
-pl = plot(plots, layout; options = Dict(
+using PlotlyJS
+
+options = Dict(
         :showSendToCloud=>true, 
         :plotlyServerURL=>"https://chart-studio.plotly.com"
-        ))
+        )
+
+# Plot the amplitude of the FRF.
+umidAmpl = abs.(U1[midpointdof, :])/phun("MM")
+# Define the layout of the figure.
+layout = Layout(;width=650, height=600, xaxis=attr(title="Frequency [Hz]", type = "log"), yaxis=attr(title="Midpoint  displacement amplitude [mm]", type = "linear"), title = "Thin plate midpoint Amplitude FRF")
+# Create the graphs:
+plots = cat(scatter(;x=vec(frequencies), y=vec(umidAmpl), mode="lines", name = "FEA", line_color = "rgb(215, 15, 15)"); dims = 1)
+# Plot the graphs:
+pl = plot(plots, layout; options)
 display(pl)
 
+
+# Plot the FRF real and imaginary components.
 umidReal = real.(U1[midpointdof, :])/phun("MM")
 umidImag = imag.(U1[midpointdof, :])/phun("MM")
-# @pgf _a = SemiLogXAxis({
-#     xlabel = "Frequency [Hz]",
-#     ylabel = "Displacement amplitude [mm]",
-#     grid="major",
-#     legend_pos  = "south east",
-#     title = "Thin plate midpoint Real/Imag FRF"
-# },
-# Plot({"red", mark="triangle"}, Table([:x => vec(frequencies), :y => vec(umidReal)])), LegendEntry("real"),
-# Plot({"blue", mark="circle"}, Table([:x => vec(frequencies), :y => vec(umidImag)])), LegendEntry("imag"))
-# display(_a)
-
+# Define the layout of the figure.
 layout = Layout(;width=650, height=600, xaxis=attr(title="Frequency [Hz]", type = "log"), yaxis=attr(title="Midpoint  displacement amplitude [mm]", type = "linear"), title = "Thin plate midpoint Real/Imag FRF")
-# Plot the graphs:
+# Create the graphs:
 plots = cat(scatter(;x=vec(frequencies), y=vec(umidReal), mode="markers+lines", name = "real", line_color = "rgb(215, 15, 15)"), scatter(;x=vec(frequencies), y=vec(umidImag), mode="markers+lines", name = "imag", line_color = "rgb(15, 15, 215)"); dims = 1)
-pl = plot(plots, layout; options = Dict(
-        :showSendToCloud=>true, 
-        :plotlyServerURL=>"https://chart-studio.plotly.com"
-        ))
+# Plot the graphs:
+pl = plot(plots, layout; options)
 display(pl)
 
-umidPhase = atan.(umidImag,umidReal)/pi*180 
 
-# @pgf _a = SemiLogXAxis({
-#     xlabel = "Frequency [Hz]",
-#     ylabel = "Phase shift [deg]",
-#     grid="major",
-#     legend_pos  = "south east",
-#     title = "Thin plate midpoint Real/Imag FRF"
-# },
-# Plot({"red", mark="triangle"}, Table([:x => vec(frequencies), :y => vec(umidPhase)])), LegendEntry("imag"))
-# display(_a)
-
+# Plot the shift of the FRF.
+umidPhase = atan.(umidImag, umidReal)/pi*180 
+# Define the layout of the figure.
 layout = Layout(;width=650, height=600, xaxis=attr(title="Frequency [Hz]", type = "log"), yaxis=attr(title="Phase shift [deg]", type = "linear"), title = "Thin plate midpoint FRF phase")
-# Plot the graphs:
+# Create the graphs:
 plots = cat(scatter(;x=vec(frequencies), y=vec(umidPhase), mode="lines", name = "phase", line_color = "rgb(15, 215, 15)"),; dims = 1)
-pl = plot(plots, layout; options = Dict(
-        :showSendToCloud=>true, 
-        :plotlyServerURL=>"https://chart-studio.plotly.com"
-        ))
+# Plot the graphs:
+pl = plot(plots, layout; options)
 display(pl)
 
 true
