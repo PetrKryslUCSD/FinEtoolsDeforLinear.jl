667a770d79daf2679d10b44d132acbef4a7ac079
Petr Krysl <pkrysl@ucsd.edu>
2022-04-02 19:11:01 -0700

    dependencies

--- examples/statics/3-d/stubby_corbel_examples.jl
+++ examples/statics/3-d/stubby_corbel_examples.jl
@@ -5,6 +5,9 @@ using FinEtoolsDeforLinear
 using FinEtoolsDeforLinear.AlgoDeforLinearModule: linearstatics, exportstresselementwise, exportstress
 using Statistics: mean
 using LinearAlgebra: Symmetric, cholesky
+using SparseArrays
+using Printf
+using SymRCM
 
 # Isotropic material
 E=1000.0;
@@ -90,7 +93,97 @@ function stubby_corbel_H8_by_hand()
     # stressfields = ElementalField[modeldata["postprocessing"]["exported"][1]["field"]]
 
     true
+end # stubby_corbel_H8_by_hand
 
+function stubby_corbel_H8_big(n = 30)
+    elementtag = "H8"
+    println("""
+    Stubby corbel example. Element: $(elementtag)
+    """)
+
+    fremem = Base.Sys.free_memory()
+    @printf "Initial free memory = %.1f [MB]\n" round(fremem/1024^2, digits = 1)
+
+    fens,fes = H8block(W, L, H, n, 2*n, 2*n)
+    println("Number of elements: $(count(fes))")
+    remfremem = fremem - Base.Sys.free_memory()
+    @printf "After mesh, free memory decrease = %.1f [MB]\n" round(remfremem/1024^2, digits = 1)
+    bfes = meshboundary(fes)
+    # end cross-section surface  for the shear loading
+    sectionL = selectelem(fens, bfes; facing=true, direction = [0.0 +1.0 0.0])
+    # 0 cross-section surface  for the reactions
+    section0 = selectelem(fens, bfes; facing=true, direction = [0.0 -1.0 0.0])
+    # 0 cross-section surface  for the reactions
+    sectionlateral = selectelem(fens, bfes; facing=true, direction = [1.0 0.0 0.0])
+
+    MR = DeforModelRed3D
+    material = MatDeforElastIso(MR, 0.0, E, nu, CTE)
+
+    # Material orientation matrix
+    csmat = [i==j ? one(FFlt) : zero(FFlt) for i=1:3, j=1:3]
+
+    function updatecs!(csmatout::FFltMat, XYZ::FFltMat, tangents::FFltMat, fe_label::FInt)
+        copyto!(csmatout, csmat)
+    end
+
+
+    geom = NodalField(fens.xyz)
+    u = NodalField(zeros(size(fens.xyz,1), 3)) # displacement field
+
+    # Renumber the nodes
+    femm = FEMMBase(IntegDomain(fes, GaussRule(3, 2)))
+    C = connectionmatrix(femm, count(fens))
+    perm = symrcm(C)
+
+    femm = FEMMDeforLinear(MR, IntegDomain(fes, GaussRule(3, 2)), material)
+
+    lx0 = connectednodes(subset(bfes, section0))
+    setebc!(u,lx0,true,1,0.0)
+    setebc!(u,lx0,true,2,0.0)
+    setebc!(u,lx0,true,3,0.0)
+    lx1 = connectednodes(subset(bfes, sectionlateral))
+    setebc!(u,lx1,true,1,0.0)
+    applyebc!(u)
+    numberdofs!(u, perm)
+    println("u.nfreedofs = $(u.nfreedofs)")
+
+    fi = ForceIntensity(FFlt, 3, getfrcL!);
+    el2femm = FEMMBase(IntegDomain(subset(bfes, sectionL), GaussRule(2, 2)))
+    F2 = distribloads(el2femm, geom, u, fi, 2);
+    associategeometry!(femm, geom)
+    remfremem = fremem - Base.Sys.free_memory()
+    @printf "Before stiffness, free memory decrease = %.1f [MB]\n" round(remfremem/1024^2, digits = 1)
+    K = stiffness(femm, geom, u)
+    remfremem = fremem - Base.Sys.free_memory()
+    @printf "After stiffness, free memory decrease = %.1f [MB]\n" round(remfremem/1024^2, digits = 1)
+    println("Stiffness: number of non zeros = $(nnz(K)) [ND]")
+
+    @time K = cholesky(Symmetric(K))
+    remfremem = fremem - Base.Sys.free_memory()
+    @printf "After cholesky, free memory decrease = %.1f [MB]\n" round(remfremem/1024^2, digits = 1)
+    @time U = K\(F2)
+    remfremem = fremem - Base.Sys.free_memory()
+    @printf "After solution, free memory decrease = %.1f [MB]\n" round(remfremem/1024^2, digits = 1)
+    scattersysvec!(u,U[:])
+    # @show length(U)
+    Tipl = selectnode(fens, box=[0 W L L 0 H], inflate=htol)
+    utip = mean(u.values[Tipl, 3], dims=1)
+    # println("Deflection: $(utip), compared to $(uzex)")
+
+    # File =  "stubby_corbel_H8_by_hand.vtk"
+    # vtkexportmesh(File, fens, fes;  vectors=[("u", u.values)])
+    # @async run(`"paraview.exe" $File`)
+
+    # modeldata["postprocessing"] = FDataDict("file"=>"hughes_cantilever_stresses_$(elementtag)", "outputcsys"=>CSys(3, 3, updatecs!), "quantity"=>:Cauchy, "component"=>[5])
+    # modeldata = exportstresselementwise(modeldata)
+
+    # modeldata["postprocessing"] = FDataDict("file"=>"hughes_cantilever_stresses_$(elementtag)",
+    # "outputcsys"=>CSys(3, 3, updatecs!), "quantity"=>:Cauchy,
+    # "component"=>collect(1:6))
+    # modeldata = exportstresselementwise(modeldata)
+    # stressfields = ElementalField[modeldata["postprocessing"]["exported"][1]["field"]]
+
+    true
 end # stubby_corbel_H8_by_hand
 
 function allrun()
@@ -101,3 +194,5 @@ function allrun()
 end # function allrun
 
 end # module stubby_corbel_examples
+
+    stubby_corbel_examples.stubby_corbel_H8_big()
