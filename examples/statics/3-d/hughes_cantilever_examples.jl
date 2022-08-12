9123451d7b42662825db290ea622492f86d4c85a
Petr Krysl <pkrysl@ucsd.edu>
2022-08-11 21:05:34 -0700

    update the hughes example

--- examples/statics/3-d/hughes_cantilever_examples.jl
+++ examples/statics/3-d/hughes_cantilever_examples.jl
@@ -1050,4 +1050,9 @@ function allrun()
     return true
 end # function allrun
 
-end # module hughes_cantilever_examples
+@info "All examples may be executed with "
+println("using .$(@__MODULE__); $(@__MODULE__).allrun()")
+
+
+end # module 
+nothing
