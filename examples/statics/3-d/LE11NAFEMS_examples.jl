a71951eed69c19395bb19960bf5080339c06f349
Petr Krysl <pkrysl@ucsd.edu>
2022-08-15 08:45:32 -0700

    update LE11NAFEMS

--- examples/statics/3-d/LE11NAFEMS_examples.jl
+++ examples/statics/3-d/LE11NAFEMS_examples.jl
@@ -260,4 +260,9 @@ function allrun()
     return true
 end # function allrun
 
-end # module LE11NAFEMS_examples
+@info "All examples may be executed with "
+println("using .$(@__MODULE__); $(@__MODULE__).allrun()")
+
+
+end # module 
+nothing
