195bfc5d5e79283531fb435b065263a6d03e40b5
Petr Krysl <pkrysl@ucsd.edu>
2022-08-15 09:55:12 -0700

    update material spectrum example

--- examples/material/material_eigen_examples.jl
+++ examples/material/material_eigen_examples.jl
@@ -426,4 +426,9 @@ function allrun()
     return true
 end # function allrun
 
-end # module material_eigen_examples
+@info "All examples may be executed with "
+println("using .$(@__MODULE__); $(@__MODULE__).allrun()")
+
+
+end # module 
+nothing
