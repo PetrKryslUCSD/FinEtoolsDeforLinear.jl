4ebc67324706a50aa5e9848760b66f398f33d7b3
Petr Krysl <pkrysl@ucsd.edu>
2024-01-30 14:27:01 -0800

    update year

--- src/FinEtoolsDeforLinear.jl
+++ src/FinEtoolsDeforLinear.jl
@@ -1,5 +1,5 @@
 """
-FinEtoolsDeforLinear (C) 2017-2019, Petr Krysl
+FinEtoolsDeforLinear (C) 2017-2024, Petr Krysl
 
 Finite Element tools.  Julia implementation  of the finite element method
 for continuum mechanics. Package for linear stress analysis problems.
