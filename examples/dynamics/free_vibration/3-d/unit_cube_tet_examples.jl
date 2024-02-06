73df2c840ac726dd02d46b521233639284362a57
Petr Krysl <pkrysl@ucsd.edu>
2024-02-06 13:38:30 -0800

    update the unit vibration example

--- examples/dynamics/free_vibration/3-d/unit_cube_tet_examples.jl
+++ examples/dynamics/free_vibration/3-d/unit_cube_tet_examples.jl
@@ -6,6 +6,7 @@ using SubSIt: ssit
 using LinearAlgebra
 using Arpack
 using DataDrop
+using VibrationGEPHelpers
 
 E = 1 * phun("PA")
 nu = 0.499
@@ -18,7 +19,7 @@ na = n1
 nb = n1
 nh = n1
 neigvs = 20                   # how many eigenvalues
-OmegaShift = (0.01 * 2 * pi)^2
+OmegaShift = (0.1 * 2 * pi)^2
 
 function unit_cube_esnice()
     println("""
@@ -44,20 +45,20 @@ function unit_cube_esnice()
     K = stiffness(femm, geom, u)
     M = mass(femm, geom, u)
 
-    DataDrop.store_matrix("unit_cube_tet-$n1.h5", "/K", K)
-    DataDrop.store_matrix("unit_cube_tet-$n1.h5", "/M", M)
+    # DataDrop.store_matrix("unit_cube_tet-$n1.h5", "/K", K)
+    # DataDrop.store_matrix("unit_cube_tet-$n1.h5", "/M", M)
 
     @time d, v, nconv = eigs(K + OmegaShift * M, M; nev = neigvs, which = :SM)
     d = d .- OmegaShift
     fs = real(sqrt.(complex(d))) / (2 * pi)
     println("Eigenvalues: $fs [Hz]")
-    DataDrop.store_matrix("unit_cube_tet-$n1.h5", "frequencies", fs)
+    # DataDrop.store_matrix("unit_cube_tet-$n1.h5", "frequencies", fs)
 
     mode = 17
     scattersysvec!(u, v[:, mode])
     File = "unit_cube_esnice.vtk"
     vtkexportmesh(File, fens, fes; vectors = [("mode$mode", u.values)])
-    @async run(`"paraview.exe" $File`)
+    #@async run(`"paraview.exe" $File`)
     true
 end # unit_cube_esnice
 
@@ -93,7 +94,49 @@ function unit_cube_esnice_ssit()
     scattersysvec!(u, v[:, mode])
     File = "unit_cube_esnice.vtk"
     vtkexportmesh(File, fens, fes; vectors = [("mode$mode", u.values)])
-    @async run(`"paraview.exe" $File`)
+    #@async run(`"paraview.exe" $File`)
+    true
+end # unit_cube_esnice
+
+function unit_cube_esnice_helpers()
+    println("""
+    Vibration modes of unit cube  of almost incompressible material.
+
+    Reference: Puso MA, Solberg J (2006) A stabilized nodally integrated
+    tetrahedral. International Journal for Numerical Methods in
+    Engineering 67: 841-867.
+    """)
+
+    MR = DeforModelRed3D
+    fens, fes = T4block(a, b, h, na, nb, nh)
+
+    geom = NodalField(fens.xyz)
+    u = NodalField(zeros(size(fens.xyz, 1), 3)) # displacement field
+
+    numberdofs!(u)
+
+    material = MatDeforElastIso(MR, rho, E, nu, 0.0)
+
+    femm = FEMMDeforLinearESNICET4(MR, IntegDomain(fes, NodalSimplexRule(3)), material)
+    associategeometry!(femm, geom)
+    K = stiffness(femm, geom, u)
+    M = mass(femm, geom, u)
+    @time d, v, nconv = VibrationGEPHelpers.gep_smallest(K + OmegaShift * M, M, neigvs; method=:ArnoldiMethod)
+    d = d .- OmegaShift
+    fs = real(sqrt.(complex(d))) / (2 * pi)
+    println("Eigenvalues: $fs [Hz]")
+    
+    @show VibrationGEPHelpers.check_K_orthogonality(d, v, K)
+    @show VibrationGEPHelpers.check_M_orthogonality(v, M)
+    
+    File = "unit_cube_esnice_helpers-ArnoldiMethod.vtk"
+    vectors = []
+    for mode in eachindex(fs)
+        scattersysvec!(u, v[:, mode])
+        push!(vectors, ("mode#$mode", deepcopy(u.values)))
+    end
+    vtkexportmesh(File, fens, fes; vectors = vectors)
+    #@async run(`"paraview.exe" $File`)
     true
 end # unit_cube_esnice
 
@@ -104,6 +147,9 @@ function allrun()
     println("#####################################################")
     println("# unit_cube_esnice_ssit ")
     unit_cube_esnice_ssit()
+    println("#####################################################")
+    println("# unit_cube_esnice_helpers ")
+    unit_cube_esnice_helpers()
     return true
 end # function allrun
 
