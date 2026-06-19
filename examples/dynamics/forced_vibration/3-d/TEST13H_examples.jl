3d8f94f67381c54e519986e57e860214d277bb1f
Petr Krysl <pkrysl@ucsd.edu>
2026-06-18 19:15:05 -0700

    add structural damping example

--- examples/dynamics/forced_vibration/3-d/TEST13H_examples.jl
+++ examples/dynamics/forced_vibration/3-d/TEST13H_examples.jl
@@ -6,7 +6,7 @@ using LinearAlgebra
 using Arpack
 using PGFPlotsX
 
-function TEST13H_hva()
+function TEST13H_hva_ray()
     # Harmonic forced vibration problem is solved for a homogeneous square plate,
     # simply-supported on the circumference.
     # This is the TEST 13H from the Abaqus v 6.12 Benchmarks manual.
@@ -111,7 +111,7 @@ function TEST13H_hva()
 
     F_f = vector_blocked(F, nfreedofs(u))[:f]
 
-    U_f = zeros(Complex{Flowat64}, nfreedofs(u), length(frequencies))
+    U_f = zeros(Complex{Float64}, nfreedofs(u), length(frequencies))
     for k in eachindex(frequencies)
         frequency = frequencies[k]
         omega = 2 * pi * frequency
@@ -179,12 +179,185 @@ function TEST13H_hva()
     display(_a)
 
     true
-end # TEST13H_hva
+end # TEST13H_hva_ray
+
+function TEST13H_hva_str()
+    # Harmonic forced vibration problem is solved for a homogeneous square plate,
+    # simply-supported on the circumference.
+    # This is the TEST 13H from the Abaqus v 6.12 Benchmarks manual.
+    # The test is recommended by the National Agency for Finite Element Methods and Standards (U.K.):
+    # Test 13 from NAFEMS “Selected Benchmarks for Forced Vibration,” R0016, March 1993.
+    #
+    #
+    # The plate is discretized with hexahedral solid elements. The simple support
+    # condition is approximated by distributed rollers on the boundary.
+    # Because only the out of plane displacements are prevented, the structure
+    # has three rigid body modes in the plane of the plate.
+    #
+    #
+    # The nonzero benchmark frequencies are (in hertz): 2.377, 5.961, 5.961,
+    # 9.483, 12.133, 12.133, 15.468, 15.468 [Hz].
+    # 
+    # In this benchmark the damping is structural rather than Rayleigh.
+
+    println("""
+    Homogeneous square plate, simply-supported on the circumference from
+    the test 13 from NAFEMS “Selected Benchmarks for Forced Vibration,” R0016, March 1993.
+    The nonzero benchmark frequencies are (in hertz): 2.377, 5.961, 5.961,
+    9.483, 12.133, 12.133, 15.468, 15.468 [Hz]. Structural damping.
+    """)
+
+    # t0 = time()
+
+    E = 200 * phun("GPa")# Young's modulus
+    nu = 0.3# Poisson ratio
+    rho = 8000 * phun("KG*M^-3")# mass density
+    qmagn = 100.0 * phun("Pa")
+    L = 10.0 * phun("M") # side of the square plate
+    t = 0.05 * phun("M") # thickness of the square plate
+    nL = 16
+    nt = 4
+    tolerance = t / nt / 100
+    # neigvs = 11;
+    # OmegaShift = (2*pi*0.5) ^ 2; # to resolve rigid body modes
+    frequencies = vcat(linearspace(0.0, 2.377, 20), linearspace(2.377, 15.0, 70))
+
+    # Compute the parameters of Rayleigh damping. For the two selected
+    # frequencies we have the relationship between the damping ratio and
+    # the Rayleigh parameters
+    # $\xi_m=a_0/\omega_m+a_1\omega_m$
+    # where $m=1,2$.  Solving for the Rayleigh parameters $a_0,a_1$ yields:
+    zeta1 = 0.02
+    loss_factor = 2*zeta1
+
+    MR = DeforModelRed3D
+    fens, fes = H8block(L, L, t, nL, nL, nt)
+
+    geom = NodalField(fens.xyz)
+    u = NodalField(zeros(Complex{Float64}, size(fens.xyz, 1), 3)) # displacement field
+    nl = selectnode(fens, box = [0.0 0.0 -Inf Inf -Inf Inf], inflate = tolerance)
+    setebc!(u, nl, true, 3)
+    nl = selectnode(fens, box = [L L -Inf Inf -Inf Inf], inflate = tolerance)
+    setebc!(u, nl, true, 3)
+    nl = selectnode(fens, box = [-Inf Inf 0.0 0.0 -Inf Inf], inflate = tolerance)
+    setebc!(u, nl, true, 3)
+    nl = selectnode(fens, box = [-Inf Inf L L -Inf Inf], inflate = tolerance)
+    setebc!(u, nl, true, 3)
+    applyebc!(u)
+    numberdofs!(u)
+    println("nfreedofs = $(nfreedofs(u))")
+
+    material = MatDeforElastIso(MR, rho, E, nu, 0.0)
+
+    femm = FEMMDeforLinearMSH8(MR, IntegDomain(fes, GaussRule(3, 2)), material)
+    femm = associategeometry!(femm, geom)
+    K = stiffness(femm, geom, u)
+    femm = FEMMDeforLinear(MR, IntegDomain(fes, GaussRule(3, 3)), material)
+    M = mass(femm, geom, u)
+    C = loss_factor * K
+
+    K_ff = matrix_blocked(K, nfreedofs(u), nfreedofs(u))[:ff]
+    M_ff = matrix_blocked(M, nfreedofs(u), nfreedofs(u))[:ff]
+    C_ff = matrix_blocked(C, nfreedofs(u), nfreedofs(u))[:ff]
+
+    # if true
+    #     t0 = time()
+    #     d,v,nev,nconv = eigs(K+OmegaShift*M, M; nev=neigvs, which=:SM)
+    #     d = d - OmegaShift;
+    #     fs = real(sqrt.(complex(d)))/(2*pi)
+    #     println("Reference Eigenvalues: $fs [Hz]")
+    #     println("eigs solution ($(time() - t0) sec)")
+    # end
+
+    bdryfes = meshboundary(fes)
+    topbfl = selectelem(fens, bdryfes, facing = true, direction = [0.0 0.0 1.0])
+    el1femm = FEMMBase(IntegDomain(subset(bdryfes, topbfl), GaussRule(2, 2)))
+    function pfun(forceout::Vector{T}, XYZ, tangents, feid, qpid) where {T}
+        forceout .= [0.0, 0.0, -qmagn]
+        return forceout
+    end
+    fi = ForceIntensity(Float64, 3, pfun)
+    F = distribloads(el1femm, geom, u, fi, 2)
+
+    F_f = vector_blocked(F, nfreedofs(u))[:f]
+
+    U_f = zeros(Complex{Float64}, nfreedofs(u), length(frequencies))
+    for k in eachindex(frequencies)
+        frequency = frequencies[k]
+        omega = 2 * pi * frequency
+        U_f[:, k] = (-omega^2 * M_ff + 1im * C_ff + K_ff) \ F_f
+    end
+
+    midpoint = selectnode(fens, box = [L / 2 L / 2 L / 2 L / 2 0 0], inflate = tolerance)
+    midpointdof = u.dofnums[midpoint, 3]
+
+    umidAmpl = abs.(U_f[midpointdof, :]) / phun("MM")
+    @pgf _a = SemiLogXAxis(
+        {
+            xlabel = "Frequency [Hz]",
+            ylabel = "Midpoint  displacement amplitude [mm]",
+            grid = "major",
+            legend_pos = "south east",
+            title = "Thin plate midpoint Amplitude FRF",
+        },
+        Plot(
+            {"red", mark = "triangle"},
+            Table([:x => vec(frequencies), :y => vec(umidAmpl)]),
+        ),
+        LegendEntry("FEA"),
+    )
+    display(_a)
+
+    umidReal = real.(U_f[midpointdof, :]) / phun("MM")
+    umidImag = imag.(U_f[midpointdof, :]) / phun("MM")
+    @pgf _a = SemiLogXAxis(
+        {
+            xlabel = "Frequency [Hz]",
+            ylabel = "Displacement amplitude [mm]",
+            grid = "major",
+            legend_pos = "south east",
+            title = "Thin plate midpoint Real/Imag FRF",
+        },
+        Plot(
+            {"red", mark = "triangle"},
+            Table([:x => vec(frequencies), :y => vec(umidReal)]),
+        ),
+        LegendEntry("real"),
+        Plot(
+            {"blue", mark = "circle"},
+            Table([:x => vec(frequencies), :y => vec(umidImag)]),
+        ),
+        LegendEntry("imag"),
+    )
+    display(_a)
+
+    umidPhase = atan.(umidImag, umidReal) / pi * 180
+    @pgf _a = SemiLogXAxis(
+        {
+            xlabel = "Frequency [Hz]",
+            ylabel = "Phase shift [deg]",
+            grid = "major",
+            legend_pos = "south east",
+            title = "Thin plate midpoint Real/Imag FRF",
+        },
+        Plot(
+            {"red", mark = "triangle"},
+            Table([:x => vec(frequencies), :y => vec(umidPhase)]),
+        ),
+        LegendEntry("imag"),
+    )
+    display(_a)
+
+    true
+end # TEST13H_hva_ray
 
 function allrun()
     println("#####################################################")
-    println("# TEST13H_hva ")
-    TEST13H_hva()
+    println("# TEST13H_hva_ray ")
+    TEST13H_hva_ray()
+    println("#####################################################")
+    println("# TEST13H_hva_str ")
+    TEST13H_hva_str()
     return true
 end # function allrun
 
